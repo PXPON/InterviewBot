c60e62ed8b314312a06eb01511c8b96e33ae4432
Paxton Perry <paxton.perry@protonmail.com>
2025-09-02 22:01:18 -0700

    Adding a new main Python file

---
+++ src/main.py
@@ -0,0 +1,10 @@
+import json
+
+# For this code we want to go through each question and have 
+# a voice recite the question.
+
+# Then, we record the interviewee giving their answer.
+# That person would have their eyes tracked and their diction
+# evaluated and later scored.
+
+
