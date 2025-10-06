02983932e826d36cdd18c3bf6b0aa61601ff2772
Paxton <paxton.perry@protonmail.com>
2025-10-06 16:54:03 -0700

    Adding requirements.txt and the evaluate_interview file to main for some reason.

--- README.md
+++ README.md
@@ -2,3 +2,30 @@
 In this application I want a tool to recite common interview questions back to me and I can film myself answering them.
 
 I would then have an AI evaluate my performance in each interview - Going over what I said as well as my facial expressions and eye movements.
+<<<<<<< Updated upstream
+=======
+
+## Create a virtual enviornment
+
+Run
+`python -m venv venv`
+
+### Activate in Windows
+
+`venv\Scripts\activate`
+
+### Activate in Linux
+
+`source venv/bin/activate`
+
+We can also try virtualvenv
+`virtualenv venv`
+
+## Install Dependencies
+
+Just run:
+`pip install -r requirements.txt`
+
+## Run InterviewBot
+
+>>>>>>> Stashed changes
---
+++ src/evaluate_interview/evaluate_interview.py
@@ -0,0 +1,32 @@
+# Creating this file to extract text from interviews
+# NOTE: This must run in Linux or iOS
+
+import datetime
+import ffmpeg
+import whisper
+# from datetime import datetime
+
+# We need to check for the GPU as a future feature
+
+model = whisper.load_model('base.en')
+option = whisper.DecodingOptions(language='en', fp16=False)
+
+file_path = input("Please enter the path to the video file: ")
+
+result = model.transcribe(file_path)
+
+print(result)
+print(result['text'])
+
+timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+save_target = f"transcription-{timestamp}.vtt"
+
+
+with open(save_target, 'w') as file:
+    for i, segment in enumerate(result['segments']):
+        file.write(str(i + 1) + '\n')
+        file.write(str(datetime.timedelta(seconds=segment['start'])) + ' --> ' + str(datetime.timedelta(seconds=segment['end'])) + '\n')
+        file.write(segment['text'].strip() + '\n')
+        file.write('\n')
+
+result['segments'][0]
